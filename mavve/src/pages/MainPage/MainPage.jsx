e3225e9abf531bed98b2186e87960f5f36d5102b
Gabin Lee <gb94@ewhain.net>
2025-07-07 19:51:52 +0900

    💄 공통컴포넌트 상단바 퍼블리싱

--- mavve/src/pages/MainPage/MainPage.jsx
+++ mavve/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import * as S from '../MainPage/MainPage.style';
+import TopBar from '../../components/Common/TopBar';
 
 export default function MainPage() {
   return (
-    <div>MainPage</div>
+    <S.Container>
+      <TopBar />
+    </S.Container>
   )
 }
--- mavve/src/pages/MainPage/MainPage.style.js
+++ mavve/src/pages/MainPage/MainPage.style.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+    background: #CAEDFF;
+    width: 100vw;
+    height: 100vh;
 `;
