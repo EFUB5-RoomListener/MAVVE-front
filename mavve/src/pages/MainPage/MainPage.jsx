cf25101ff0f86bccf25a8b2b36c4bfbd4080d32c
Gabin Lee <gb94@ewhain.net>
2025-07-07 21:23:39 +0900

    💄 공통컴포넌트 사이드바 퍼블리싱

--- mavve/src/pages/MainPage/MainPage.jsx
+++ mavve/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import * as S from '../MainPage/MainPage.style';
 import TopBar from '../../components/Common/TopBar';
+import SideBar from '../../components/Common/SideBar';
 
 export default function MainPage() {
   return (
     <S.Container>
       <TopBar />
+      <S.Contents>
+        <SideBar />
+      </S.Contents>
     </S.Container>
   )
 }
--- mavve/src/pages/MainPage/MainPage.style.js
+++ mavve/src/pages/MainPage/MainPage.style.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
     background: #CAEDFF;
     width: 100vw;
-    height: 100vh;
+`;
+
+export const Contents = styled.div`
+    display: flex;
+    padding: 0.5rem 0rem 0rem 1rem;
 `;
