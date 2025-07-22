9e7e7d5466e93564320171a84d137032b261504c
wys0530 <youn.soo.woo@gmail.com>
2025-07-22 15:22:19 +0900

    💄 MyPage 내 상세 페이지 퍼블리싱

--- mavve/src/pages/MyPage/LikedRoomPage.jsx
+++ mavve/src/pages/MyPage/LikedRoomPage.jsx
@@ -2,8 +2,118 @@ import React, { useState } from "react";
 import * as S from "./MyPage.style";
 import TopBar from "../../components/Common/TopBar";
 import SideBar from "../../components/Common/SideBar";
+import RoomComponent from "../../components/Common/RoomComponent";
 
 export default function LikedRoomPage() {
+  const [myRooms, setMyRooms] = useState([
+    {
+      id: 1,
+      title: "1번 방",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 2,
+      title: "222",
+      tag: "차분한",
+      duration: "00:00:10",
+      liked: true,
+      likes: 2,
+    },
+    {
+      id: 3,
+      title: "333",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 4,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 5,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 6,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 7,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 8,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 9,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 10,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 11,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 12,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 13,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+  ]);
+
+  const likedRooms = myRooms.filter((room) => room.liked);
+
   return (
     <S.Container>
       <S.TopBarContainer>
@@ -14,9 +124,14 @@ export default function LikedRoomPage() {
           <SideBar />
         </S.SidebarContainer>
         <S.Main>
-          <S.ViewArea>
+          <S.PageHeader>
             <S.Title>내가 좋아하는 방</S.Title>
-          </S.ViewArea>
+          </S.PageHeader>
+          <S.PageRoomContainer>
+            {likedRooms.map((room) => (
+              <RoomComponent key={room.id} data={room} />
+            ))}
+          </S.PageRoomContainer>
         </S.Main>
       </S.MainContainer>
     </S.Container>
--- mavve/src/pages/MyPage/MyRoomPage.jsx
+++ mavve/src/pages/MyPage/MyRoomPage.jsx
@@ -1,9 +1,149 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./MyPage.style";
 import TopBar from "../../components/Common/TopBar";
 import SideBar from "../../components/Common/SideBar";
+import RoomComponent from "../../components/Common/RoomComponent";
+import CreateRoomBtn from "../../assets/MyPage/createRoomBtn.svg";
+import CreateRoomBtnHover from "../../assets/MyPage/createRoomBtnHover.svg";
 
 export default function MyPage() {
+  const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
+
+  //개발 테스트용, 추후 API 연결 시 삭제
+  const [myRooms, setMyRooms] = useState([
+    {
+      id: 1,
+      title: "1번 방",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 2,
+      title: "222",
+      tag: "차분한",
+      duration: "00:00:10",
+      liked: true,
+      likes: 2,
+    },
+    {
+      id: 3,
+      title: "333",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 4,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 5,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 6,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 7,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 8,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 9,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: true,
+      likes: 200,
+    },
+    {
+      id: 10,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 11,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 12,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 13,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    //이 페이지에서만 임시 추가
+    {
+      id: 13,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 13,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+    {
+      id: 13,
+      title: "나만의 방1",
+      tag: "신나는",
+      duration: "01:24:34",
+      liked: false,
+      likes: 200,
+    },
+  ]);
+
   return (
     <S.Container>
       <S.TopBarContainer>
@@ -14,9 +154,21 @@ export default function MyPage() {
           <SideBar />
         </S.SidebarContainer>
         <S.Main>
-          <S.ViewArea>
+          <S.PageHeader>
             <S.Title>내가 만든 방</S.Title>
-          </S.ViewArea>
+            <S.CreateRoomBtn
+              src={isHovered ? CreateRoomBtnHover : CreateRoomBtn}
+              alt="방 만들기"
+              onClick={() => navigate("/room")}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            />
+          </S.PageHeader>
+          <S.PageRoomContainer>
+            {myRooms.map((room) => (
+              <RoomComponent key={room.id} data={room} />
+            ))}
+          </S.PageRoomContainer>
         </S.Main>
       </S.MainContainer>
     </S.Container>
