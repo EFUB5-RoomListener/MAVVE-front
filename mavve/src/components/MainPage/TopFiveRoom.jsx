c9ad700e84baf92de50c5f1c93c158e56ae840b7
Gabin Lee <gb94@ewhain.net>
2025-07-22 16:21:12 +0900

    💄 메인페이지 퍼블리싱 완료

--- mavve/src/components/MainPage/TopFiveRoom.jsx
+++ mavve/src/components/MainPage/TopFiveRoom.jsx
@@ -1,10 +1,77 @@
 import React from 'react'
 import * as S from '../MainPage/TopFiveRoom.style'
+import popularRoom from '../../assets/MainPage/PopularRoom.svg'
+import pause from '../../assets/MainPage/Pause.svg'
 
 export default function TopFiveRoom() {
   return (
     <S.Container>
         <S.Title>현재 인기 방 TOP 5</S.Title>
+        <S.Contents>
+            <img src={popularRoom} alt="topfiveroom" />
+            <S.Rooms>
+                <S.Room1>
+                    <S.RoomTitle>방 제목</S.RoomTitle>
+                    <S.Tags>
+                        <S.Hashtag>#신나는</S.Hashtag>
+                        <S.Hashtag>#락밴드</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                    </S.Tags>
+                </S.Room1>
+                <S.Room2>
+                    <S.RoomTitle>방 제목</S.RoomTitle>
+                    <S.Tags>
+                        <S.Hashtag>#신나는</S.Hashtag>
+                        <S.Hashtag>#락밴드</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                    </S.Tags>
+                </S.Room2>
+                <S.Room3>
+                    <S.Room3Info>
+                        <S.RoomTitle>방 제목</S.RoomTitle>
+                        <S.Tags>
+                            <S.Hashtag>#신나는</S.Hashtag>
+                            <S.Hashtag>#락밴드</S.Hashtag>
+                            <S.Hashtag>#리듬</S.Hashtag>
+                            <S.Hashtag>#리듬</S.Hashtag>
+                        </S.Tags>
+                    </S.Room3Info>
+                    <S.PlayBar>
+                        <S.PauseButton>
+                            <img src={pause} alt='pause_button'/>
+                        </S.PauseButton>
+                        <S.SongInfo>
+                            <S.SongText>Foo Fighters</S.SongText>
+                            <S.SongText>Everlong</S.SongText>
+                            <S.SongText>04:10</S.SongText>
+                        </S.SongInfo>
+                    </S.PlayBar>
+                </S.Room3>
+                <S.Room4>
+                    <S.RoomTitle>방 제목</S.RoomTitle>
+                    <S.Tags>
+                        <S.Hashtag>#신나는</S.Hashtag>
+                        <S.Hashtag>#락밴드</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                    </S.Tags>
+                </S.Room4>
+                <S.Room5>
+                    <S.RoomTitle>방 제목</S.RoomTitle>
+                    <S.Tags>
+                        <S.Hashtag>#신나는</S.Hashtag>
+                        <S.Hashtag>#락밴드</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                        <S.Hashtag>#리듬</S.Hashtag>
+                    </S.Tags>
+                </S.Room5>
+            </S.Rooms>
+            <S.CD>
+                <S.Hole />
+            </S.CD>
+        </S.Contents>
     </S.Container>
   )
 }
--- mavve/src/pages/MainPage/MainPage.jsx
+++ mavve/src/pages/MainPage/MainPage.jsx
@@ -4,20 +4,20 @@ import TopBar from '../../components/Common/TopBar';
 import SideBar from '../../components/Common/SideBar';
 import TopFiveRoom from '../../components/MainPage/TopFiveRoom';
 import Diaries from '../../components/MainPage/Diaries';
-import RoomComponent from '../../components/Common/RoomComponent';
-import OneLineNote from '../../components/Common/OneLineNote';
+import wave from '../../assets/MainPage/wave.svg';
 
 export default function MainPage() {
   return (
     <S.Container>
+      <S.Wave>
+            <img src={wave} alt="background_wave" />
+      </S.Wave>
       <TopBar />
       <S.Contents>
         <SideBar />
         <S.MainContents>
           <TopFiveRoom />
           <Diaries />
-          <RoomComponent />
-          <OneLineNote />
         </S.MainContents>
       </S.Contents>
     </S.Container>
