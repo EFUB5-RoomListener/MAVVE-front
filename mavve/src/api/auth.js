cf79ee402a06631e693a9016b1f113ec47ae002c
wys0530 <youn.soo.woo@gmail.com>
2025-08-04 01:16:27 +0900

    Revert "🐛 로그인 에러 수정"
    This reverts commit 5ff7aef0a19180d8b3e7f3dbe2b94969f03d32a3.

--- mavve/src/api/auth.js
+++ mavve/src/api/auth.js
@@ -1,19 +1,19 @@
 import axiosInstance from "./axiosInstance";
 
 // 로그인
-// export const login = async (code) => {
-//   const res = await axiosInstance.post(
-//     "/auth/login",
-//     { code },
-//     {
-//       headers: { "Content-Type": "application/json" },
-//       withCredentials: true,
-//       validateStatus: () => true,
-//       responseType: "text",
-//     }
-//   );
-//   return res;
-// };
+export const login = async (code) => {
+  const res = await axiosInstance.post(
+    "/auth/login",
+    { code },
+    {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+      validateStatus: () => true,
+      responseType: "text",
+    }
+  );
+  return res;
+};
 
 // 로그아웃
 export const logout = async () => {
